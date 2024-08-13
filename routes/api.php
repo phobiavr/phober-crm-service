9354820a753df27c5d4c6a699a11abfdd17454fa
Abdukhaligov <hikmat.pou@gmail.com>
2024-08-13 19:05:37 +0400

    remove: unnecessary files and dependencies / refactor code styles

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth.server')->get('/', function () {
+Route::middleware('auth.server')->get('', function () {
     return Auth::guard('server')->user();
 });
-
-
